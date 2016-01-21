5dafab9251221d25e0648ce90a156d82f8dcb10e
Carlos Leguizamon <carlos.alberto.leguizamon@gmail.com>
2016-01-21 20:00:44 -0300

    Adds 02_exampleFindElements

--- src/test/java/com/learn/selenium/SeleniumTest.java
+++ src/test/java/com/learn/selenium/SeleniumTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * Selenium simple tests examples
  */
@@ -15,7 +17,7 @@ public class SeleniumTest {
     private WebDriver driver = new FirefoxDriver();
 
     @Test
-    public void example1() {
+    public void exampleFindElement() {
         driver.get("https://www.wikipedia.org/");
         WebElement webElement = driver.findElement(By.linkText("English"));
         webElement.click();
@@ -23,4 +25,22 @@ public class SeleniumTest {
         String errorMessage = "By clicking the 'English' link in the home page the page was not redirected correctly";
         Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, errorMessage);
     }
+
+    @Test
+    public void exampleFindElements() {
+        driver.get("https://www.wikipedia.org/");
+        //You should learn how to locate elements by xpath :)
+        //The xpath used will match all the language links
+        List<WebElement> listOfLanguagesLinks = driver.findElements(By.xpath("//div[@class='central-featured']//a/strong"));
+        for (WebElement languageLink : listOfLanguagesLinks) {
+            if(languageLink.getText().equals("Italiano")){
+                languageLink.click();
+                break;
+            }
+        }
+        String expectedUrl = "https://it.wikipedia.org/wiki/Pagina_principale";
+        String errorMessage = "By clicking the 'Italiano' link in the home page the page was not redirected correctly";
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, errorMessage);
+    }
+
 }
